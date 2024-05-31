ef1da4e63fc03e145b96f915161605cb6f501558
Siphephelo Khanyile <siphephelokhanyile@gmail.com>
2024-05-31 23:03:02 +0200

    fix: task 3, add return statement

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,7 +1,7 @@
 import { uploadPhoto, createUser } from './utils';
 
 function handleProfileSignup() {
-  Promise.all([uploadPhoto(), createUser()])
+  return Promise.all([uploadPhoto(), createUser()])
     .then((messsages) => {
       console.log(`${messsages[0].body} ${messsages[1].firstName} ${messsages[1].lastName}`);
     })
