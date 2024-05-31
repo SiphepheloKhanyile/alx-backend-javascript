5a6ab1fb0e9bc8d108764c4dd8295be2a4bf8ca2
Siphephelo Khanyile <siphephelokhanyile@gmail.com>
2024-05-31 23:00:08 +0200

    add: task 3

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,11 @@
+import { uploadPhoto, createUser } from './utils';
+
+function handleProfileSignup() {
+  Promise.all([uploadPhoto(), createUser()])
+    .then((messsages) => {
+      console.log(`${messsages[0].body} ${messsages[1].firstName} ${messsages[1].lastName}`);
+    })
+    .catch(() => console.log('Signup system offline'));
+}
+
+export default handleProfileSignup;
