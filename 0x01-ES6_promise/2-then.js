e2e4a082ad8dcfd12dc10e213b4b690f849281f4
Siphephelo Khanyile <siphephelokhanyile@gmail.com>
2024-05-31 22:05:55 +0200

    fix: task 2

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,22 +1,13 @@
-function handleResponseFromAPI(promise) {
-  // try {
-  //   const awaitPromise = await promise();
-  //   console.log('Got a response from the API');
-  //   return {
-  //     status: 200,
-  //     body: 'success',
-  //   };
-  // } catch (error) {
-  //   return Error(error);
-  // }
-
-  promise.then(() => {
-    console.log('Got a response from the API');
-    return {
-      status: 200,
-      bod: 'success',
-    };
-  }).catch(Error);
+async function handleResponseFromAPI(promise) {
+  return promise
+    .then(() => {
+      console.log('Got a response from the API');
+      return {
+        status: 200,
+        body: 'success',
+      };
+    })
+    .catch(() => { Error(); });
 }
 
 export default handleResponseFromAPI;
