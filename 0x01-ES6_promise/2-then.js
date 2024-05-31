38d8377c0e450690727e06c2dbf12a706d22942a
Siphephelo Khanyile <siphephelokhanyile@gmail.com>
2024-05-31 22:27:49 +0200

    fix: task refactored code

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,14 +1,8 @@
 function handleResponseFromAPI(promise) {
   return promise
-    .finally(() => {
-      console.log('Got a response from the API');
-      return {
-        status: 200,
-        body: 'success',
-      };
-    })
-    // eslint-disable-next-line no-new
-    .catch(() => { new Error(); });
+    .then(() => ({ status: 200, body: 'success' }))
+    .catch(() => new Error())
+    .finally(() => console.log('Got a response from the API'));
 }
 
 export default handleResponseFromAPI;
