8cf2c52dc8806b1ff4b3a4811c294ba14c122344
Siphephelo Khanyile <siphephelokhanyile@gmail.com>
2024-06-02 01:06:12 +0200

    add: task 7

---
+++ 0x02-ES6_classes/7-airport.js
@@ -0,0 +1,15 @@
+class Airport {
+  constructor(name, code) {
+    // eslint-disable-next-line no-underscore-dangle
+    this._name = name;
+    // eslint-disable-next-line no-underscore-dangle
+    this._code = code;
+  }
+
+  get [Symbol.toStringTag]() {
+    // eslint-disable-next-line no-underscore-dangle
+    return `${this._code}`;
+  }
+}
+
+export default Airport;
