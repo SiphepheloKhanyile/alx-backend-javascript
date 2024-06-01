9f39b64ab2adcff1d69b2a2fa6d14fd2a27eaef1
Siphephelo Khanyile <siphephelokhanyile@gmail.com>
2024-06-02 01:10:55 +0200

    add: task 8

---
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -0,0 +1,19 @@
+class HolbertonClass {
+  constructor(size, location) {
+    // eslint-disable-next-line no-underscore-dangle
+    this._size = size;
+    // eslint-disable-next-line no-underscore-dangle
+    this._location = location;
+  }
+
+  [Symbol.toPrimitive](hint) {
+    if (hint === 'number') {
+      // eslint-disable-next-line no-underscore-dangle
+      return this._size;
+    }
+    // eslint-disable-next-line no-underscore-dangle
+    return this._location;
+  }
+}
+
+export default HolbertonClass;
