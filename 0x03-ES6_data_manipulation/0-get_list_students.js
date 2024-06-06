bf4b002dc071315980a843086c5f31198c73bb7b
Siphephelo Khanyile <siphephelokhanyile@gmail.com>
2024-06-06 02:43:51 +0200

    add: advanced task 0

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,12 @@
+// eslint-disable-next-line space-before-blocks
+function getListStudents(){
+  const students = [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+  ];
+
+  return students;
+}
+
+export default getListStudents;
