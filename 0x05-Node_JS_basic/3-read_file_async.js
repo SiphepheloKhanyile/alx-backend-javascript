21de76f42d5d612889242c76287103204acde8f4
Siphephelo Khanyile <siphephelokhanyile@gmail.com>
2024-07-24 02:42:02 +0200

    add: 3. Reading a file asynchronously with Node JS

---
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,41 @@
+const fs = require('fs').promises; // Use promises API
+
+async function countStudents(filePath) {
+  try {
+    const data = await fs.readFile(filePath, 'utf8'); // Read file asynchronously
+    const dataArray = data.split('\n');
+
+    const sweStudents = [];
+    let numberOfSweStudents = 0;
+    let numberOfCsStudents = 0;
+    let numberOfStudents = 0;
+    const csStudents = [];
+
+    dataArray.forEach((element) => {
+      if (element !== '') {
+        numberOfStudents += 1;
+      }
+      const student = element.split(',');
+      if (student[student.length - 1] === 'SWE') {
+        sweStudents.push(student[0]);
+        numberOfSweStudents += 1;
+      }
+
+      if (student[student.length - 1] === 'CS') {
+        csStudents.push(student[0]);
+        numberOfCsStudents += 1;
+      }
+    });
+
+    // Return a Promise that resolves with the counts and lists
+    console.log(`Number of students: ${numberOfStudents - 1}`);
+    console.log(`Number of students in CS: ${numberOfCsStudents}. List: ${csStudents.join(', ').trimEnd()}`);
+    console.log(`Number of students in SWE: ${numberOfSweStudents}. List: ${sweStudents.join(', ').trimEnd()}`);
+    return data;
+  } catch (error) {
+    // Reject the Promise with the error
+    throw new Error('Cannot load the database');
+  }
+}
+
+module.exports = countStudents;
