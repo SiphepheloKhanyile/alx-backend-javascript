4cc80d23cd5147473f2e18986235501beb20622c
Siphephelo Khanyile <siphephelokhanyile@gmail.com>
2024-07-24 04:34:14 +0200

    5. Create a more complex HTTP server using Node's HTTP module

---
+++ 0x05-Node_JS_basic/5-http.js
@@ -0,0 +1,61 @@
+const fs = require('fs');
+const http = require('http');
+const url = require('url');
+
+const app = http.createServer((req, res) => {
+  const { path } = url.parse(req.url);
+  res.writeHead(200, { 'Content-type': 'text/plain' });
+
+  if (path === '/') {
+    res.end('Hello Holberton School!');
+  }
+
+  if (path === '/students') {
+    if (!process.argv[2]) {
+      res.end('This is the list of our students');
+    }
+
+    const databaseName = process.argv[2];
+    console.log(databaseName);
+
+    try {
+      fs.readFile(databaseName, (err, data = '') => {
+        if (err) {
+          return res.end('This is the list of our students');
+        }
+        const dataArray = data.toString().split('\n');
+        const sweStudents = [];
+        let numberOfSweStudents = 0;
+        let numberOfCsStudents = 0;
+        let numberOfStudents = 0;
+        const csStudents = [];
+
+        dataArray.forEach((element) => {
+          if (element !== '') {
+            numberOfStudents += 1;
+          }
+          const student = element.split(',');
+          if (student[student.length - 1] === 'SWE') {
+            sweStudents.push(student[0]);
+            numberOfSweStudents += 1;
+          }
+
+          if (student[student.length - 1] === 'CS') {
+            csStudents.push(student[0]);
+            numberOfCsStudents += 1;
+          }
+        });
+
+        res.write('This is the list of our students\n');
+        res.write(`Number of students: ${numberOfStudents - 1}\n`);
+        res.write(`Number of students in CS: ${numberOfCsStudents}. List: ${csStudents.join(', ').trimEnd()}\n`);
+        return res.end(`Number of students in SWE: ${numberOfSweStudents}. List: ${sweStudents.join(', ').trimEnd()}`);
+      });
+    } catch (error) {
+      console.log('Cannot load the database');
+    }
+  }
+});
+
+app.listen(1245);
+module.exports = app;
